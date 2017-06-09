dc53d463c3e0e4618aeb469bd19bb77e7916118c
Mason Banning <mason.banning@gmail.com>
2017-06-09 23:29:59 +0200

    launch the game from main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import ui.MainScreen;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MainScreen screen = new MainScreen();
+        screen.setVisible(true);
     }
 }
